ef36b7027cbc4e4e044b61c8d300458f9d1d8765
Huy Hoang <goaty1992@outlook.com>
2018-07-25 01:06:49 -0400

    some more benchmarking here

--- IonDotnet.Bench/SerializerGround.cs
+++ IonDotnet.Bench/SerializerGround.cs
@@ -1,40 +1,96 @@
 ﻿using System;
-using System.IO;
-using System.Linq;
-using IonDotnet.Internals.Binary;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 using IonDotnet.Serialization;
+using Newtonsoft.Json;
 
 namespace IonDotnet.Bench
 {
     // ReSharper disable once UnusedMember.Global
     public class SerializerGround : IRunable
     {
-        private class SimplePoco
+        public class SimplePoco
         {
             public string Name { get; set; }
             public int Age { get; set; }
+            public string Nickname { get; set; }
+            public int Id { get; set; }
+            public bool IsHandsome { get; set; }
+        }
+
+        [MemoryDiagnoser]
+        public class Benchmark
+        {
+            public static SimplePoco[] Data = GenerateArray();
+
+            public static SimplePoco[] GenerateArray()
+            {
+                var random = new Random();
+                return new[]
+                {
+                    new SimplePoco
+                    {
+                        Name = "Bob",
+                        Age = random.Next(0, int.MaxValue / 5),
+                        Nickname = Guid.NewGuid().ToString(),
+                        IsHandsome = true,
+                        Id = random.Next(0, int.MaxValue / 3)
+                    },
+                    new SimplePoco
+                    {
+                        Name = "Adam",
+                        Age = random.Next(0, int.MaxValue / 5),
+                        Nickname = Guid.NewGuid().ToString(),
+                        IsHandsome = false,
+                        Id = random.Next(0, int.MaxValue / 3)
+                    },
+                    new SimplePoco
+                    {
+                        Name = "Jason",
+                        Age = random.Next(0, int.MaxValue / 5),
+                        Nickname = Guid.NewGuid().ToString(),
+                        IsHandsome = false,
+                        Id = random.Next(0, int.MaxValue / 3)
+                    },
+                    new SimplePoco
+                    {
+                        Name = "Helen",
+                        Age = random.Next(0, int.MaxValue / 5),
+                        Nickname = Guid.NewGuid().ToString(),
+                        IsHandsome = true,
+                        Id = random.Next(0, int.MaxValue / 3)
+                    }
+                };
+            }
+
+            [Benchmark]
+            public void JsonDotnet()
+            {
+                JsonConvert.SerializeObject(Data);
+            }
+
+            [Benchmark]
+            public void IonDotnet()
+            {
+                var serializer = new IonSerializer();
+                serializer.Serialize(Data);
+            }
         }
 
         public void Run(ArraySegment<string> args)
         {
-            var serializer = new IonSerializer();
-            var output = serializer.Serialize(new SimplePoco
-            {
-                Name = "huy",
-                Age = 26
-            });
-            var s = string.Join(",", output.Select(b => $"0x{b:x2}"));
-            Console.WriteLine(s);
-
-            var reader = new UserBinaryReader(new MemoryStream(output));
-            Console.WriteLine(reader.MoveNext());
-            reader.StepIn();
-            Console.WriteLine(reader.MoveNext());
-            Console.WriteLine(reader.CurrentFieldName);
-            Console.WriteLine(reader.StringValue());
-            Console.WriteLine(reader.MoveNext());
-            Console.WriteLine(reader.CurrentFieldName);
-            Console.WriteLine(reader.IntValue());
+            BenchmarkRunner.Run<Benchmark>();
+//            var serializer = new IonSerializer();
+//
+//            var listPoco =
+//                var output = serializer.Serialize(listPoco);
+////            Console.WriteLine(string.Join(",", output.Select(b => $"0x{b:x2}")));
+//
+//            var jsonOutput = JsonConvert.SerializeObject(listPoco);
+//            Console.WriteLine(jsonOutput);
+//            var jsonBytes = Encoding.UTF8.GetBytes(jsonOutput);
+//            Console.WriteLine($"json Length {jsonBytes.Length}");
+//            Console.WriteLine($"ion length {output.Length}");
         }
     }
 }
