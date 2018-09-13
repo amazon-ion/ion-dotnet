b4b32e4b5b0b933c4454f93c268788cf094e60cc
hdhoang <goaty1992@outlook.com>
2018-09-13 15:50:31 -0400

    fix number tests

--- IonDotnet.Bench/IonExpressionText.cs
+++ IonDotnet.Bench/IonExpressionText.cs
@@ -10,9 +10,6 @@ namespace IonDotnet.Bench
             var sw = new StringWriter();
             var writer = new IonTextWriter(sw);
             var action = IonSerializerExpression.GetAction<T>();
-            // var action = GetAction<T>();
-            //now write
-            byte[] bytes = null;
             action(obj, writer);
             writer.Finish();
             return sw.ToString();
