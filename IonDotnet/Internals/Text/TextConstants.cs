1d28efb6a70213e2b524428854013c9b80ddd663
Arman A <araman@bitquilltech.com>
2019-12-04 14:48:51 -0800

    minor change in error message

--- IonDotnet/Internals/Text/TextConstants.cs
+++ IonDotnet/Internals/Text/TextConstants.cs
@@ -267,7 +267,7 @@ namespace IonDotnet.Internals.Text
             if (h >= 'A' && h <= 'F')
                 return h - 'A' + 10;
 
-            throw new InvalidTokenException($"Invalid Hex character: { h }");
+            throw new InvalidTokenException($"Invalid Hex value: { h }");
         }
 
         public static bool IsValidSymbolCharacter(int c)
