772931568c6d4d88b8c83578697197ae960b7935
Armanbqt <armana@bitquilltech.com>
2020-01-17 15:03:23 -0800

    Removed obsolete tests

--- IonDotnet.Tests/Tree/IonContainerTest.cs
+++ IonDotnet.Tests/Tree/IonContainerTest.cs
@@ -71,7 +71,6 @@ namespace IonDotnet.Tests.Tree
             foreach (var c in v)
             {
                 Assert.IsTrue(v.Contains(c));
-                //Assert.AreEqual(c.Container, v);
             }
 
             //clear
@@ -79,7 +78,6 @@ namespace IonDotnet.Tests.Tree
             foreach (var c in list)
             {
                 Assert.IsFalse(v.Contains(c));
-                //Assert.IsNull(c.Container);
             }
         }
 
@@ -105,7 +103,6 @@ namespace IonDotnet.Tests.Tree
             foreach (var c in list)
             {
                 Assert.IsFalse(v.Contains(c));
-                //Assert.IsNull(c.Container);
             }
         }
 
@@ -140,10 +137,8 @@ namespace IonDotnet.Tests.Tree
                 DoAdd(v, c);
             }
 
-            //Assert.AreEqual(v, r?.Container);
             Assert.IsTrue(v.Contains(r));
             v.Remove(r);
-            //Assert.IsNull(r?.Container);
             Assert.AreEqual(count - 1, v.Count);
         }
     }
