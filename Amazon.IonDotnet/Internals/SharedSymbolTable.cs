67054121cd0610f15cb477a0570b6ddf180df52e
Simon Zhao <simonz@bitquilltech.com>
2020-03-30 17:16:42 -0700

    Apply general styling fixes to SharedSymbolTable

--- Amazon.IonDotnet/Internals/SharedSymbolTable.cs
+++ Amazon.IonDotnet/Internals/SharedSymbolTable.cs
@@ -13,15 +13,15 @@
  * permissions and limitations under the License.
  */
 
-using System;
-using System.Collections.Generic;
-using Amazon.IonDotnet.Internals.Binary;
-
 namespace Amazon.IonDotnet.Internals
 {
+    using System;
+    using System.Collections.Generic;
+    using Amazon.IonDotnet.Internals.Binary;
+
     /// <inheritdoc />
     /// <summary>
-    /// An immutable, thread-safe shared symbol table. Used for system table
+    /// An immutable, thread-safe shared symbol table. Used for system table.
     /// </summary>
     internal sealed class SharedSymbolTable : ISymbolTable
     {
@@ -37,10 +37,13 @@ namespace Amazon.IonDotnet.Internals
             SystemSymbols.Imports,
             SystemSymbols.Symbols,
             SystemSymbols.MaxId,
-            SystemSymbols.IonSharedSymbolTable
+            SystemSymbols.IonSharedSymbolTable,
         };
 
         private static readonly ISymbolTable Ion10SystemSymtab;
+
+        private readonly IDictionary<string, int> symbolsMap;
+        private readonly string[] symbolNames;
 
         static SharedSymbolTable()
         {
@@ -53,9 +56,6 @@ namespace Amazon.IonDotnet.Internals
             Ion10SystemSymtab = new SharedSymbolTable(SystemSymbols.Ion, 1, SystemSymbolsArray, map);
         }
 
-        private readonly IDictionary<string, int> _symbolsMap;
-        private readonly string[] _symbolNames;
-
         private SharedSymbolTable(string name, int version, List<string> symbolNames, IDictionary<string, int> symbolsMap)
             : this(name, version, symbolNames.ToArray(), symbolsMap)
         {
@@ -63,38 +63,52 @@ namespace Amazon.IonDotnet.Internals
 
         private SharedSymbolTable(string name, int version, string[] symbolNames, IDictionary<string, int> symbolsMap)
         {
-            Name = name;
-            Version = version;
-            _symbolsMap = symbolsMap;
-            _symbolNames = symbolNames;
+            this.Name = name;
+            this.Version = version;
+            this.symbolsMap = symbolsMap;
+            this.symbolNames = symbolNames;
         }
 
         public string Name { get; }
+
         public int Version { get; }
+
         public bool IsLocal { get; } = false;
+
         public bool IsShared { get; } = true;
+
         public bool IsSubstitute { get; } = false;
-        public bool IsSystem => Name == SystemSymbols.Ion;
+
+        public bool IsSystem => this.Name == SystemSymbols.Ion;
+
         public bool IsReadOnly { get; } = true;
-
-        public void MakeReadOnly()
-        {
-            //already is
-        }
-
-        public ISymbolTable GetSystemTable() => IsSystem ? this : null;
 
         public string IonVersionId
         {
             get
             {
-                if (!IsSystem)
+                if (!this.IsSystem)
+                {
                     return null;
-                if (Version != 1)
-                    throw new IonException($"Unrecognized version {Version}");
+                }
+
+                if (this.Version != 1)
+                {
+                    throw new IonException($"Unrecognized version {this.Version}");
+                }
+
                 return SystemSymbols.Ion10;
             }
         }
+
+        public int MaxId => this.symbolNames.Length;
+
+        public void MakeReadOnly()
+        {
+            // IsReadOnly is always true
+        }
+
+        public ISymbolTable GetSystemTable() => this.IsSystem ? this : null;
 
         IReadOnlyList<ISymbolTable> ISymbolTable.GetImportedTables()
         {
@@ -103,40 +117,50 @@ namespace Amazon.IonDotnet.Internals
 
         public int GetImportedMaxId() => 0;
 
-        public int MaxId => _symbolNames.Length;
-
         public SymbolToken Intern(string text)
         {
             if (text == null)
+            {
                 throw new ArgumentNullException(nameof(text));
+            }
 
-            var symtok = Find(text);
+            var symtok = this.Find(text);
             if (symtok == default)
+            {
                 throw new InvalidOperationException("Table is read-only");
+            }
+
             return symtok;
         }
 
         public SymbolToken Find(string text)
         {
             if (text is null)
+            {
                 throw new ArgumentNullException(nameof(text));
+            }
 
-            if (!_symbolsMap.TryGetValue(text, out var sid))
+            if (!this.symbolsMap.TryGetValue(text, out var sid))
+            {
                 return default;
+            }
 
-            var internedText = _symbolNames[sid - 1];
+            var internedText = this.symbolNames[sid - 1];
             return new SymbolToken(internedText, SymbolToken.UnknownSid);
         }
 
         public int FindSymbolId(string name)
-            => _symbolsMap.TryGetValue(name, out var sid) ? sid : SymbolToken.UnknownSid;
+            => this.symbolsMap.TryGetValue(name, out var sid) ? sid : SymbolToken.UnknownSid;
 
         public string FindKnownSymbol(int sid)
         {
             if (sid < 0)
+            {
                 return null;
+            }
+
             var offset = sid - 1;
-            return sid != 0 && offset < _symbolNames.Length ? _symbolNames[offset] : null;
+            return sid != 0 && offset < this.symbolNames.Length ? this.symbolNames[offset] : null;
         }
 
         public void WriteTo(IIonWriter writer)
@@ -144,29 +168,39 @@ namespace Amazon.IonDotnet.Internals
             writer.WriteValues(new SymbolTableReader(this));
         }
 
-        public IEnumerable<string> GetDeclaredSymbolNames() => _symbolNames;
+        public IEnumerable<string> GetDeclaredSymbolNames() => this.symbolNames;
 
         /// <summary>
-        /// Get the system symbol table
+        /// Get the system symbol table.
         /// </summary>
-        /// <param name="version">Ion version</param>
-        /// <returns>System symbol table</returns>
-        // ReSharper disable once ParameterOnlyUsedForPreconditionCheck.Global
+        /// <param name="version">Ion version.</param>
+        /// <returns>System symbol table.</returns>
         internal static ISymbolTable GetSystem(int version)
         {
             if (version != 1)
+            {
                 throw new ArgumentException("only Ion 1.0 system symbols are supported");
+            }
+
             return Ion10SystemSymtab;
         }
 
         internal static ISymbolTable NewSharedSymbolTable(string name, int version, ISymbolTable priorSymtab, IEnumerable<string> symbols)
         {
             if (string.IsNullOrWhiteSpace(name))
+            {
                 throw new ArgumentNullException(nameof(name), "Must not be empty");
+            }
+
             if (symbols == null)
+            {
                 throw new ArgumentNullException(nameof(symbols), "Must not be null");
+            }
+
             if (version < 1)
+            {
                 throw new ArgumentException("Must be at least 1", nameof(version));
+            }
 
             var (symbolList, symbolMap) = PrepSymbolListAndMap(priorSymtab, symbols);
             return new SharedSymbolTable(name, version, symbolList, symbolMap);
@@ -196,7 +230,10 @@ namespace Amazon.IonDotnet.Internals
             foreach (var symbol in symbols)
             {
                 if (symbolMap.ContainsKey(symbol))
+                {
                     continue;
+                }
+
                 symbolMap[symbol] = sid;
                 symbolList.Add(symbol);
                 sid++;
