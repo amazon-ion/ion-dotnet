e492bd40a1025b7fb0b050873f656f7dfae8e9cb
Simon Zhao <simonz@bitquilltech.com>
2020-03-31 17:37:23 -0700

    Apply general styling fixes to SimpleCatalog

--- Amazon.IonDotnet/Utils/SimpleCatalog.cs
+++ Amazon.IonDotnet/Utils/SimpleCatalog.cs
@@ -13,37 +13,39 @@
  * permissions and limitations under the License.
  */
 
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using static System.Diagnostics.Debug;
-
 namespace Amazon.IonDotnet.Utils
 {
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using static System.Diagnostics.Debug;
+
     /// <inheritdoc cref="ICatalog" />
     /// <summary>
     /// A basic implementation for a mutable catalog.
     /// </summary>
     public class SimpleCatalog : IMutableCatalog, IEnumerable<ISymbolTable>
     {
-        private class MaxFirstComparer : IComparer<int>
-        {
-            public int Compare(int x, int y) => y.CompareTo(x);
-        }
-
         private static readonly MaxFirstComparer Comparer = new MaxFirstComparer();
 
-        private readonly Dictionary<string, SortedList<int, ISymbolTable>> _tablesByName
+        private readonly Dictionary<string, SortedList<int, ISymbolTable>> tablesByName
             = new Dictionary<string, SortedList<int, ISymbolTable>>();
 
         public ISymbolTable GetTable(string name)
         {
-            if (string.IsNullOrEmpty(name)) throw new ArgumentNullException(nameof(name));
-            SortedList<int, ISymbolTable> versions;
-            lock (_tablesByName)
+            if (string.IsNullOrEmpty(name))
             {
-                if (!_tablesByName.TryGetValue(name, out versions)) return null;
+                throw new ArgumentNullException(nameof(name));
+            }
+
+            SortedList<int, ISymbolTable> versions;
+            lock (this.tablesByName)
+            {
+                if (!this.tablesByName.TryGetValue(name, out versions))
+                {
+                    return null;
+                }
             }
 
             lock (versions)
@@ -54,23 +56,36 @@ namespace Amazon.IonDotnet.Utils
 
         public ISymbolTable GetTable(string name, int version)
         {
-            if (string.IsNullOrEmpty(name)) throw new ArgumentNullException(nameof(name));
-            if (version < 1) throw new ArgumentException("Must be >=1", nameof(version));
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+
+            if (version < 1)
+            {
+                throw new ArgumentException("Must be >=1", nameof(version));
+            }
 
             SortedList<int, ISymbolTable> versions;
-            lock (_tablesByName)
+            lock (this.tablesByName)
             {
-                if (!_tablesByName.TryGetValue(name, out versions)) return null;
+                if (!this.tablesByName.TryGetValue(name, out versions))
+                {
+                    return null;
+                }
             }
 
             lock (versions)
             {
-                if (versions.TryGetValue(version, out var table)) return table;
+                if (versions.TryGetValue(version, out var table))
+                {
+                    return table;
+                }
 
-                Assert(versions.Count > 0);
+                Assert(versions.Count > 0, "Count is not > than 0");
                 var bestMatch = GetBestMatchOfVersion(version, versions.Keys);
                 table = versions[bestMatch];
-                Assert(table != null);
+                Assert(table != null, "table is null");
                 return table;
             }
         }
@@ -82,17 +97,17 @@ namespace Amazon.IonDotnet.Utils
                 throw new ArgumentException("table must be shared, non-subtitute", nameof(sharedTable));
             }
 
-            //make a local copy
+            // make a local copy
             var name = sharedTable.Name;
             var version = sharedTable.Version;
-            Assert(version > 0);
+            Assert(version > 0, "version is not > 0");
 
-            lock (_tablesByName)
+            lock (this.tablesByName)
             {
-                if (!_tablesByName.TryGetValue(name, out var versions))
+                if (!this.tablesByName.TryGetValue(name, out var versions))
                 {
                     versions = new SortedList<int, ISymbolTable>(Comparer);
-                    _tablesByName.Add(name, versions);
+                    this.tablesByName.Add(name, versions);
                 }
 
                 lock (versions)
@@ -105,10 +120,10 @@ namespace Amazon.IonDotnet.Utils
         public IEnumerator<ISymbolTable> GetEnumerator()
         {
             List<ISymbolTable> tables;
-            lock (_tablesByName)
+            lock (this.tablesByName)
             {
-                tables = new List<ISymbolTable>(_tablesByName.Count);
-                foreach (var kvp in _tablesByName)
+                tables = new List<ISymbolTable>(this.tablesByName.Count);
+                foreach (var kvp in this.tablesByName)
                 {
                     var versions = kvp.Value;
                     lock (versions)
@@ -121,27 +136,34 @@ namespace Amazon.IonDotnet.Utils
             return tables.GetEnumerator();
         }
 
-        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
 
         private static int GetBestMatchOfVersion(int requestedVersion, IEnumerable<int> versionsKeys)
         {
-            //no idea what's going on here
             var best = requestedVersion;
             var ibest = -1;
             foreach (var available in versionsKeys)
             {
-                Assert(available != requestedVersion);
+                Assert(available != requestedVersion, "available is not the requestedVersion");
                 var v = available;
 
                 if (requestedVersion < best)
                 {
-                    if (requestedVersion >= v || v >= best) continue;
+                    if (requestedVersion >= v || v >= best)
+                    {
+                        continue;
+                    }
+
                     best = v;
                     ibest = available;
                 }
                 else if (best < requestedVersion)
                 {
-                    if (best >= v) continue;
+                    if (best >= v)
+                    {
+                        continue;
+                    }
+
                     best = v;
                     ibest = available;
                 }
@@ -153,6 +175,11 @@ namespace Amazon.IonDotnet.Utils
             }
 
             return ibest;
+        }
+
+        private class MaxFirstComparer : IComparer<int>
+        {
+            public int Compare(int x, int y) => y.CompareTo(x);
         }
     }
 }
