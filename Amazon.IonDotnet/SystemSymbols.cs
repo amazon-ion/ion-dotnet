d1832be0670386122faf34e248a0d16bb2692d90
Simon Zhao <simonz@bitquilltech.com>
2020-04-01 14:17:53 -0700

    Apply general styling fixes to SystemSymbols

--- Amazon.IonDotnet/SystemSymbols.cs
+++ Amazon.IonDotnet/SystemSymbols.cs
@@ -1,4 +1,19 @@
-﻿namespace Amazon.IonDotnet
+﻿/*
+ * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+namespace Amazon.IonDotnet
 {
     /// <summary>
     /// Constants for symbols defined by the Ion specification.
@@ -14,7 +29,6 @@
         /// The ID of system symbol {@value #ION}, as defined by Ion 1.0.
         /// </summary>
         public const int IonSid = 1;
-
 
         /// <summary>
         /// The text of system symbol {@value}, as defined by Ion 1.0.
@@ -61,31 +75,48 @@
         /// <returns>Symbol ID.</returns>
         public static int ResolveSidForSymbolTableField(string fieldName)
         {
-            const int shortestName = 4; //'name'
+            const int shortestName = 4; // 'name'
             if (fieldName is null || fieldName.Length < shortestName)
+            {
                 return SymbolToken.UnknownSid;
+            }
 
             switch (fieldName[0])
             {
                 case 'v':
                     if (Version.Equals(fieldName))
+                    {
                         return VersionSid;
+                    }
+
                     break;
                 case 'n':
                     if (Name.Equals(fieldName))
+                    {
                         return NameSid;
+                    }
+
                     break;
                 case 's':
                     if (Symbols.Equals(fieldName))
+                    {
                         return SymbolsSid;
+                    }
+
                     break;
                 case 'i':
                     if (Imports.Equals(fieldName))
+                    {
                         return ImportsSid;
+                    }
+
                     break;
                 case 'm':
                     if (MaxId.Equals(fieldName))
+                    {
                         return MaxIdSid;
+                    }
+
                     break;
             }
 
