57a5059f332eda2f90219ccc5a4db1e3d79576ff
Simon Zhao <simonz@bitquilltech.com>
2020-04-01 14:19:23 -0700

    Fix some minor whitespace issues

--- Amazon.IonDotnet/Tree/IIonSequence.cs
+++ Amazon.IonDotnet/Tree/IIonSequence.cs
@@ -18,7 +18,9 @@ namespace Amazon.IonDotnet.Tree
     public interface IIonSequence : IIonContainer
     {
         void RemoveAt(int index);
+
         int IndexOf(IIonValue item);
+
         IIonValue GetElementAt(int index);
     }
 }
